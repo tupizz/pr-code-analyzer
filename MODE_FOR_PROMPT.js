3c19202b47875bc064e222012f504fe3830433aa
Tadeu Tupinambá. <tadeu.tupiz@gmail.com>
2024-10-30 19:02:59 -0300

    workflow update

--- MODE_FOR_PROMPT.js
+++ MODE_FOR_PROMPT.js
@@ -6,19 +6,24 @@ module.exports = {
 
     brief: `
             Please give a brief explanation of the changes in a few sentences, be direct, less verbose. Use a simple language and add emojis to make it more fun. Don't need to add too much detail, just a brief explanation.
+            
             IMPORTANT:
             - Don't use mardowns or code formatting. The output should be a simple text.
             - Make it easy to read. And don't be too verbose.
+            - Don't be too much formal or too much casual. Be professional but friendly. The user is going to share this in a slack channel.
             - At the end of the output, add a section estimating the amount of time it will take to review the changes. 
             - Don't make the time estimation too long, it should be a rough estimate.
-                        Example:
-                        "⏱️ Estimated time to review: ~10 minutes"
+                Example:
+                "⏱️ Estimated time to review: ~10 minutes"
             `,
 
     description: `
             Please describe the changes this PR is making in a few sentences and topics please.
             - Summarize the changes into few topics, but don't be too general. Explain the changes and the reasons for them.
-            - When making sense, please add mermaid diagrams to explain the changes.
+            - When make sense create mermaid diagrams to explain the changes.
+            - When make sense create sequence diagrams explaining the flow of the changes.
+            - If there is any database changes, create a diagram to explain the changes, maybe a table schema diagram. Or ER diagram.
+            - Make sure the generated mermaid diagrams are valid.
             - Don't add too many topics, make it easy to read. a Product person should be able to read this and understand the changes.
             - Don't be too verbose. Be very direct and to the point.
             `,
